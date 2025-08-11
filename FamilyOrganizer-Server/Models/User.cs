af861d0fc5bde91c67a903a349632cf6c60a7110
Ngô Minh Tiến Đạt <datngo@Mac-mini-cua-Ngo.local>
2025-08-11 22:19:32 +0700

    add Getdebuggerdisplay string again

--- FamilyOrganizer-Server/Models/User.cs
+++ FamilyOrganizer-Server/Models/User.cs
@@ -8,6 +8,9 @@ public class User
     public string Email { get; set; } = "";
     public string Password { get; set; } = ""; 
     public string FullName { get; set; } = "";
-}
 
-    
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
